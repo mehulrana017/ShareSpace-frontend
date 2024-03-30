48043e920a8a5445728519584d54e4007073fc4e
mehul rana <mehul.syncsignature@gmail.com>
2024-03-30 15:18:46 +0530

    project init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
